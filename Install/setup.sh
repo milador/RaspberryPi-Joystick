b1a4703a4d93f3f3f1a72c9a47255e1c8c1d5e1f
milador <milador.tech@gmail.com>
2022-09-09 17:59:00 -0700

    Add develop branch to install code

--- Install/setup.sh
+++ Install/setup.sh
@@ -1,4 +1,4 @@
 cd /home/pi
 
-wget https://raw.githubusercontent.com/milador/RaspberryPi-Joystick/master/Install/install.sh
+wget https://raw.githubusercontent.com/milador/RaspberryPi-Joystick/develop/Install/install.sh
 sh ./install.sh $1
